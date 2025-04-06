ecca97f7b7a191881d7e3fb1eb9b6a5a8e588f5f
Sekhar Reddy <sekharreddyp@outlook.com>
2025-04-06 16:56:06 +0530

    Added login and email extractioin

--- WebScrapeApp/ArticleServices/Mdpi.cs
+++ WebScrapeApp/ArticleServices/Mdpi.cs
@@ -11,8 +11,20 @@ namespace WebScrapeApp.ArticleServices
 {
     public class Mdpi
     {
-        public void RunSelenium(string SearchText)
+        public void RunSelenium(string SearchText, string username = null, string password = null)
         {
+            if (string.IsNullOrEmpty(SearchText))
+            {
+                throw new Exception("Search text is empty");
+            }
+            if (string.IsNullOrEmpty(username))
+            {
+                username = "insblr001@outlook.com";
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                password = "Sekhar@123";
+            }
             //string downloadDirectory = @"C:\CustomDownloads";
             string downloadDirectory = @"C:\CustomDownloads\mdpi";
             // Ensure the directory exists
@@ -49,8 +61,30 @@ namespace WebScrapeApp.ArticleServices
                 // Maximize the browser window
                 driver.Manage().Window.Maximize();
 
-                // Wait for the page to load (optional)
+                Task.Delay(5000).Wait();
+
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"container\"]/section/header/div[1]/div/div[3]/div/a[1]")));
+
+                IWebElement signin = driver.FindElement(By.XPath("//*[@id=\"container\"]/section/header/div[1]/div/div[3]/div/a[1]"));
+                signin.Click();
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Id("username")));
+
+
+                //UA_SignInUpButton
+                //IWebElement signin = driver.FindElement(By.ClassName("UA_SignInUpButton"));
+                //signin.Click();
+                //Task.Delay(1000).Wait();
+
+                IWebElement email = driver.FindElement(By.Id("username"));
+                email.SendKeys(username);
+                IWebElement pass = driver.FindElement(By.Id("password"));
+                pass.SendKeys(password);
+                //IWebElement login = driver.FindElement(By.XPath("//button[contains(text(), 'Continue')]"));
+                pass.SendKeys(Keys.Enter);
+
+                // Wait for the page to load (optional)
+                //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
                 wait.Until(d => d.FindElement(By.Id("q")));
 
                 //try
@@ -80,7 +114,8 @@ namespace WebScrapeApp.ArticleServices
                 IWebElement search = driver.FindElement(By.Id("q"));
                 search.SendKeys(SearchText);
                 search.SendKeys(Keys.Enter);
-                Task.Delay(10000).Wait();
+                //TODO apply more filters and delay if required
+                Task.Delay(2000).Wait();
 
                 IWebElement pageInfoDiv = driver.FindElement(By.XPath("//div[contains(text(), 'Displaying article') and contains(text(), 'on page')]"));
 
@@ -102,7 +137,7 @@ namespace WebScrapeApp.ArticleServices
                     Console.WriteLine("Total pages: " + totalPages1);
                 }
 
-                for (int i = 1; i < totalPages; i++)
+                for (int i = 1; i <= totalPages; i++)
                 //foreach (var i in Enumerable.Range(1, totalPages))
                 {
                     try
@@ -138,7 +173,7 @@ namespace WebScrapeApp.ArticleServices
                         pagenumber.SendKeys(Keys.Enter);
 
                         // Wait for the next page to load
-                        Task.Delay(3000).Wait();
+                        Task.Delay(1000).Wait();
                         WriteToExcel();
                     }
                     catch (Exception ex)
@@ -191,7 +226,7 @@ namespace WebScrapeApp.ArticleServices
                         string[] columnheaders = streamreader.ReadLine().Split(delimiter);
                         foreach (string columnheader in columnheaders)
                         {
-                            datatable.Columns.Add(columnheader);
+                            datatable.Columns.Add(columnheader.Trim());
                         }
                     }
 
@@ -206,7 +241,7 @@ namespace WebScrapeApp.ArticleServices
             }
 
             // Append data to Excel file
-            using (var workbook = new XLWorkbook())
+            using (var workbook = File.Exists(excelFilePath) ? new XLWorkbook(excelFilePath) : new XLWorkbook())
             {
                 IXLWorksheet worksheet;
                 if (workbook.Worksheets.Count == 0)
@@ -215,7 +250,7 @@ namespace WebScrapeApp.ArticleServices
                 }
                 else
                 {
-                    worksheet = workbook.Worksheet(1);
+                    worksheet = workbook.Worksheet("Sheet1");
                 }
 
                 int startRow = worksheet.LastRowUsed()?.RowNumber() + 1 ?? 1;
@@ -225,7 +260,7 @@ namespace WebScrapeApp.ArticleServices
                 {
                     for (int i = 0; i < datatable.Columns.Count; i++)
                     {
-                        worksheet.Cell(startRow, i + 1).Value = datatable.Columns[i].ColumnName;
+                        worksheet.Cell(startRow, i + 1).Value = datatable.Columns[i].ColumnName.Trim();
                     }
                     startRow++;
                 }
@@ -239,9 +274,187 @@ namespace WebScrapeApp.ArticleServices
                     }
                 }
 
+                // Check if the "data" worksheet exists, if not, create it
+                IXLWorksheet dataWorksheet;
+                if (workbook.Worksheets.Any(ws => ws.Name == "data"))
+                {
+                    dataWorksheet = workbook.Worksheet("data");
+                }
+                else
+                {
+                    dataWorksheet = workbook.Worksheets.Add("data");
+                    dataWorksheet.Cell(1, 1).Value = "AUTHOR";
+                    dataWorksheet.Cell(1, 2).Value = "EMAIL";
+                    dataWorksheet.Cell(1, 3).Value = "TITLE";
+                }
+
+                int dataStartRow = dataWorksheet.LastRowUsed()?.RowNumber() + 1 ?? 2;
+
+                // Write the data to the "data" worksheet
+                for (int i = 0; i < datatable.Rows.Count; i++)
+                {
+                    string[] authors = datatable.Rows[i]["AUTHOR"].ToString().Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    string[] emails = datatable.Rows[i]["EMAIL"].ToString().Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int k = 0; k < authors.Length; k++)
+                    {
+                        dataWorksheet.Cell(dataStartRow + i + k, 1).Value = authors[k].Trim();
+                        if (k < emails.Length)
+                        {
+                            dataWorksheet.Cell(dataStartRow + i + k, 2).Value = emails[k].Trim();
+                        }
+                        dataWorksheet.Cell(dataStartRow + i + k, 3).Value = datatable.Rows[i]["TITLE"].ToString();
+                    }
+                }
+
                 workbook.SaveAs(excelFilePath);
             }
+
+            // Optionally, delete the .txt files after processing
+            foreach (string file in txtFiles)
+            {
+                File.Delete(file);
+            }
         }
+
+
+        //private void WriteToExcel()
+        //{
+        //    string downloadDirectory = @"C:\CustomDownloads\mdpi";
+        //    string excelFilePath = Path.Combine(downloadDirectory, "output.xlsx");
+
+        //    // Get all .txt files from the download directory
+        //    string[] txtFiles = Directory.GetFiles(downloadDirectory, "*.txt");
+
+        //    DataTable datatable = new DataTable();
+        //    char[] delimiter = new char[] { '\t' };
+
+        //    foreach (string file in txtFiles)
+        //    {
+        //        using (StreamReader streamreader = new StreamReader(file))
+        //        {
+        //            // Read the column headers from the first file
+        //            if (datatable.Columns.Count == 0)
+        //            {
+        //                string[] columnheaders = streamreader.ReadLine().Split(delimiter);
+        //                foreach (string columnheader in columnheaders)
+        //                {
+        //                    datatable.Columns.Add(columnheader.Trim());
+        //                }
+        //            }
+
+        //            // Read the data rows
+        //            while (streamreader.Peek() > 0)
+        //            {
+        //                DataRow datarow = datatable.NewRow();
+        //                datarow.ItemArray = streamreader.ReadLine().Split(delimiter);
+        //                datatable.Rows.Add(datarow);
+        //            }
+        //        }
+        //    }
+
+        //    // Append data to Excel file
+        //    //using (var workbook = new XLWorkbook())
+        //    using (var workbook = File.Exists(excelFilePath) ? new XLWorkbook(excelFilePath) : new XLWorkbook())
+        //    {
+        //        IXLWorksheet worksheet;
+        //        if (workbook.Worksheets.Count == 0)
+        //        {
+        //            worksheet = workbook.Worksheets.Add("Sheet1");
+        //        }
+        //        else
+        //        {
+        //            worksheet = workbook.Worksheet(1);
+        //        }
+
+        //        int startRow = worksheet.LastRowUsed()?.RowNumber() + 1 ?? 1;
+
+        //        // Write the headers if the worksheet is empty
+        //        if (startRow == 1)
+        //        {
+        //            for (int i = 0; i < datatable.Columns.Count; i++)
+        //            {
+        //                worksheet.Cell(startRow, i + 1).Value = datatable.Columns[i].ColumnName.Trim();
+        //            }
+        //            startRow++;
+        //        }
+
+        //        // Write the data
+        //        for (int i = 0; i < datatable.Rows.Count; i++)
+        //        {
+        //            for (int j = 0; j < datatable.Columns.Count; j++)
+        //            {
+        //                worksheet.Cell(startRow + i, j + 1).Value = datatable.Rows[i][j].ToString();
+        //            }
+        //        }
+
+        //        workbook.SaveAs(excelFilePath);
+
+        //        IXLWorksheet dataWorksheet;
+        //        if (workbook.Worksheets.Any(ws => ws.Name == "data"))
+        //        {
+        //            dataWorksheet = workbook.Worksheet("data");
+        //        }
+        //        else
+        //        {
+        //            dataWorksheet = workbook.Worksheets.Add("data");
+        //            dataWorksheet.Cell(1, 1).Value = "AUTHOR";
+        //            dataWorksheet.Cell(1, 2).Value = "EMAIL";
+        //            dataWorksheet.Cell(1, 3).Value = "TITLE";
+        //        }
+
+        //        worksheet = workbook.Worksheets.Add("data");
+        //        startRow = worksheet.LastRowUsed()?.RowNumber() + 1 ?? 1;
+        //        worksheet.Cell(startRow, 1).Value = "AUTHOR";
+        //        worksheet.Cell(startRow, 2).Value = "EMAIL";
+        //        worksheet.Cell(startRow, 3).Value = "TITLE";
+
+        //        // Write the data
+        //        for (int i = 0; i < datatable.Rows.Count; i++)
+        //        {
+        //            string[] authors = datatable.Rows[i]["AUTHOR"].ToString().Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+        //            string[] emails = datatable.Rows[i]["EMAIL"].ToString().Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+        //            for (int k = 0; k < authors.Length; k++)
+        //            {
+        //                worksheet.Cell(startRow + i + k, 1).Value = authors[k].Trim();
+        //                if (k < emails.Length)
+        //                {
+        //                    worksheet.Cell(startRow + i + k, 2).Value = emails[k].Trim();
+        //                }
+        //                worksheet.Cell(startRow + i + k, 3).Value = datatable.Rows[i]["TITLE"].ToString();
+        //            }
+
+        //            //for (int j = 0; j < datatable.Columns.Count; j++)
+        //            //{
+
+        //            //    if (datatable.Columns[i].ColumnName == "AUTHOR")
+        //            //    {
+        //            //        worksheet.Cell(startRow + i, 1).Value = datatable.Rows[i][j].ToString();
+        //            //    }
+        //            //    else if (datatable.Columns[i].ColumnName == "EMAIL")
+        //            //    {
+        //            //        worksheet.Cell(startRow + i, 2).Value = datatable.Rows[i][j].ToString();
+        //            //    }
+        //            //    else if (datatable.Columns[i].ColumnName == "TITLE")
+        //            //    {
+        //            //        worksheet.Cell(startRow + i, 3).Value = datatable.Rows[i][j].ToString();
+        //            //    }
+
+
+        //            //}
+        //        }
+
+        //        workbook.SaveAs(excelFilePath);
+
+        //    }
+
+        //    // Optionally, delete the .txt files after processing
+        //    foreach (string file in txtFiles)
+        //    {
+        //        File.Delete(file);
+        //    }
+        //}
 
     }
 }
